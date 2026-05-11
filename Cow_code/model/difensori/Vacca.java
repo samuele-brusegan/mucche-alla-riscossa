3f867c3cc1bed0eaabc2ab51833d1e81b2d101a4
Anna Zambelli <anna08.zambelli@gmail.com>
2026-05-11 20:12:14 +0200

    Creazione codice Vacca.java

---
+++ Cow_code/model/difensori/Vacca.java
@@ -0,0 +1,31 @@
+/** Vacca
+ * Colpisce tutti i nemici nella sua corsia con danno elevato.
+ * È la più potente (e più cara)
+ */
+public class Vacca extends UnitaBovina {
+
+    private int velocitaLancio;
+
+    private int dannoVitello;
+
+    public Vacca() {
+        super("Vacca", 200, 8.0, 200);
+        this.velocitaLancio = 15; // Molto veloce
+        this.dannoVitello   = 40;
+    }
+
+    // Il vitello lanciato attraversa tutta la corsia colpendo ogni insetto sul percorso.
+    
+    @Override
+    public void attacca() {
+        lancioDelVitello();
+    }
+
+    /** Spara un vitello verso destra nella corsia */
+    public void lancioDelVitello() {
+        System.out.println("[" + nome + "] LANCIO VITELLO! Velocità: " + velocitaLancio + " | Danno: " + dannoVitello);
+        // Todo: creare Proiettile(colpisce tutti i nemici in linea)
+    }
+
+    public int getVelocitaLancio() { return velocitaLancio; }
+    public int getDannoVitello()   { return dannoVitello; }
