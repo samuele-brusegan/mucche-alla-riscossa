87b2012d3256865bfd8c113db6f94d5092d357c2
Anna Zambelli <anna08.zambelli@gmail.com>
2026-05-11 22:14:51 +0200

    Creato codice MuccaCornuta.java

---
+++ Cow_code/model/difensori/MuccaCornuta.java
@@ -0,0 +1,28 @@
+/** MuccaCornutaAssunta
+ * Non attacca a distanza: aspetta che un insetto entri nella sua cella
+ * e poi lo incorna con danni devastanti. Ha molta vita per fare da muro.
+ */
+
+public class MuccaCornuta extends UnitaBovina {
+
+    private int dannoCarica;
+
+    public MuccaCornutaAssunta() {
+        super("Mucca Cornuta Assunta", 125, 0.5, 300);
+        this.dannoCarica = 80;
+    }
+
+    //incorna il primo insetto che si trova nella sua stessa cella o in quella adiacente.
+
+    @Override
+    public void attacca() {
+        incorna();
+    }
+
+    public void incorna() {
+        System.out.println("[" + nome + "] CORNATA! Danno corpo a corpo: " + dannoCarica);
+        // Todo: trovare InsettoMutante nella stessa cella e applicare dannoCarica
+    }
+
+    public int getDannoCarica() { return dannoCarica; }
+}
