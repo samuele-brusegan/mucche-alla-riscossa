066cb9f4b0b34d779e5459922290b6e5a80d83c9
Anna Zambelli <anna08.zambelli@gmail.com>
2026-05-11 18:42:46 +0200

    Creazione codice Mucca.java

---
+++ Cow_code/model/difensori/Mucca.java
@@ -0,0 +1,33 @@
+/**
+ * Mucca:
+ * rilascia "Boasse" sul terreno davanti a sé. 
+ * Ogni insetto che ci calpesta subisce danno.
+ */
+
+public class MuccaBeatrice extends UnitaBovina {
+
+    private double rallentamentoBoassa; /**rallentamento applicato all'insetto che la calpesta(0.0 fermo; 1.0 normale velocità) */
+
+    private int dannoTrappola;
+
+    public MuccaBeatrice() {
+        super("Mucca Beatrice", 75, 1.0, 150);
+        this.rallentamentoBoassa = 0.3; // l'insetto rallenta al 30% della sua velocità
+        this.dannoTrappola = 50;
+    }
+
+    /**attacca() per Beatrice significa: piazzare una boassa nella cella davanti. Si attiva periodicamente(ogni X secondi).*/
+    @Override
+    public void attacca() {
+        piazzaTrappola();
+    }
+
+    //La boassa rimane sul campo finché un insetto non ci cammina sopra.
+    public void piazzaTrappola() {
+        System.out.println("[" + nome + "] Boassa piazzata in colonna " + (colonna + 1) + "! Odore garanzia.");
+        // Todo: creare oggetto BoassaEsplosiva e aggiungerlo alla griglia tramite GameController
+    }
+
+    public double getRallentamentoBoassa() { return rallentamentoBoassa; }
+    public int    getDannoTrappola()       { return dannoTrappola; }
+}
